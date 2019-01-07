e5581c471005fb63d38941a529c428421ee8ed92
Peter Hwu <peterbhwu@gmail.com>
2019-01-06 17:10:22 -0800

    updated splash

--- public/Utility/AttributeCircle/AttributeCircle.mjs
+++ public/Utility/AttributeCircle/AttributeCircle.mjs
@@ -8,24 +8,11 @@ class attributeCircle extends HTMLElement {
   static get observedAttributes() {
     return ['type', 'active'];
   }
+  
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(attribute.cloneNode(true));
-  }
-
-  connectedCallback() {
-    this.addEventListener('click', this.handleClickAttribute)
-  }
-
-  disconnectedCallback() {
-    this.removeEventListener('click', this.handleClickAttribute)
-    console.log('bye')
-    
-  }
-  handleClickAttribute() {
-    document.getElementById('app-container').removeChild(document.getElementById('splash-page'))
-    this.toggleAttribute('active');
   }
 }
 
--- public/index.mjs
+++ public/index.mjs
@@ -2,32 +2,66 @@ const splash = document.createElement('splash');
 splash.innerHTML = `
   <link rel="stylesheet" type="text/css" media="screen" href="Splash.css" />
   <div id="splash">
-    <span>Welcome Adventurer!</span>
-    <span>Please Choose an Attribute:</span>
-    <div id="attributes">
-      <attribute-circle type="str"></attribute-circle>
-      <attribute-circle type="int"></attribute-circle>
-      <attribute-circle type="dex"></attribute-circle>
+    <div id="selection">
+      <span>Welcome Adventurer!</span>
+      <span>Please Choose an Attribute:</span>
+      <div id="attributes">
+        <attribute-circle type="Strength"></attribute-circle>
+        <attribute-circle type="Intelligence"></attribute-circle>
+        <attribute-circle type="Dexterity"></attribute-circle>
+      </div>
+    </div>
+    <div id="attribute-details">
+      <span class="circle"></span>
+      <div class="details">
+        <span>Assocations</span>
+      </div>
+      <span id="selected-attribute"></span>
     </div>
   </div>
 `;
 
 class splashPage extends HTMLElement {
+  static get observedAttributes() {
+    return ['selection'];
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(splash.cloneNode(true));
 
-    
   }
 
   connectedCallback() {
     document.getElementById('splash-page').appendChild(this.createAttributeScript());
-}
+    const collection = document.getElementById('splash-page').shadowRoot.getElementById('attributes').children
+    const attributes = [...collection]
+    attributes.map((child) => {
+      child.addEventListener('click', () => {
+        this.handleSelectionChoice(child.getAttribute('type'))
+      })
+    })
+  }
 
   disconnectedCallback() {
     document.head.appendChild(this.createMainScript());
     document.getElementById('app-container').appendChild(document.createElement('main-page'))
+    const collection = document.getElementById('splash-page').shadowRoot.getElementById('attributes').children
+    const attributes = [...collection]
+    attributes.map((child) => {
+      child.removeEventListener('click', () => {
+        this.handleSelectionChoice(child.getAttribute('type'))
+      })
+    })
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    const element = this.shadowRoot.getElementById('selected-attribute');
+    if (!oldVal) {
+      element.parentNode.classList.add('attribute-details-active');
+    }
+    element.innerHTML = newVal
   }
 
   createAttributeScript() {
@@ -44,6 +78,12 @@ class splashPage extends HTMLElement {
     return mainScript;
   }
 
+  handleSelectionChoice(type) {
+    this.setAttribute('selection', type)
+  }
+  confirmSelectionChoice(type) {
+    // document.getElementById('app-container').removeChild(document.getElementById('splash-page'))
+  }
 }
 
 customElements.define('splash-page', splashPage);
